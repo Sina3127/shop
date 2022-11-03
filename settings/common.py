de5236653acb9da723fa0b3af35348dcf545a141
mahdi.akbari <mahdi.akbari@cafebazaar.ir>
2022-11-03 09:15:26 +0330

    remove cache from project

--- settings/common.py
+++ settings/common.py
@@ -118,12 +118,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': 'D:\cash',
-    }
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
