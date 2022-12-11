8dfe79335e77312df33f2a2e7124f57ebbd10734
mahdi.akbari <mahdi.akbari@cafebazaar.ir>
2022-12-11 23:02:30 +0330

    add basic form

--- shop/form.py
+++ shop/form.py
@@ -1,9 +1,18 @@
-from django.forms import ModelForm
-
+from django.forms import ModelForm, Form, DateField, ModelChoiceField
+from account.models import Address
 from shop.models import ReviewComment
 
 
 class AddReview(ModelForm):
     class Meta:
         model = ReviewComment
-        fields = ('text', 'point', )
+        fields = ('text', 'point',)
+
+
+class AddTransaction(Form):
+    send_time = DateField()
+    address = ModelChoiceField(queryset=Address.objects.all())
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.address.queryset = self.address.queryset.filter(user=user)
